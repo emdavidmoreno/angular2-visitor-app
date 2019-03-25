cb6374b305f0e654302db58f9352a3ebda556a3c
David Moreno <devel.enterprise@gmail.com>
2019-03-25 19:26:32 -0400

    firebase config

---
+++ src/environments/firebase.config.dist.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBr22tyyfrKggvDoIgHORnRGR3KlmZlj7o",
+  authDomain: "your-firebase-auth-domain.firebaseapp.com",
+  databaseURL: "https://your-firebase-database-url",
+  projectId: "your-firebase-project-id",
+  storageBucket: "your-firebase-storage-bucket"
+
+}
