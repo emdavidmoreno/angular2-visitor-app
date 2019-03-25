8365d7156e30a952e44887343db7b120ad137404
David Moreno <devel.enterprise@gmail.com>
2019-03-25 19:27:44 -0400

    visitor-list component

---
+++ src/app/visitor-list/visitor-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { VisitorService } from '../services/visitor.service';
+
+@Component({
+  selector: 'app-visitor-list',
+  templateUrl: './visitor-list.component.html',
+  styleUrls: ['./visitor-list.component.css'],
+  providers:[VisitorService]
+})
+export class VisitorListComponent implements OnInit {
+  private visitors = [];
+
+  constructor(private service: VisitorService) { }
+
+  loadVisitors = () =>{
+    this.service.getAll().subscribe(data=>{
+      console.log(data);
+      this.visitors = data
+    })
+    
+  }
+
+  ngOnInit() {
+    this.loadVisitors();
+  }
+
+}
